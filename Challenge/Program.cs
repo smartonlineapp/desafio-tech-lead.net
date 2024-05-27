3d402c2277704bee4311ee9b676c28c9140aa815
Pedro H Santana <pedro.santana@onlineapp.com.br>
2024-05-27 10:02:57 -0300

    push challenge

---
+++ Challenge/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Challenge;
+
+class Program
+{
+    /*
+     * Você precisa escrever um programa em C# que verifica se uma palavra ou frase é um palíndromo.
+     * Um palíndromo é uma sequência de caracteres que é igual quando lida da esquerda para a direita
+     * e da direita para esquerda, ignorando espaços, pontuação e capitalização.
+     * Por exemplo, a palavra 'radar' é um palíndromo.
+     * O número de interações/computações, não pode ser superior ao tamanho da string.
+    */
+
+    static void Main(string[] _)
+    {
+        string input = "";
+
+        bool isPalindrome = IsPalindrome(input);
+
+        Console.WriteLine(isPalindrome);
+
+        Console.ReadKey();
+    }
+
+    static bool IsPalindrome(string str)
+    {
+        return false;
+    }
+}
